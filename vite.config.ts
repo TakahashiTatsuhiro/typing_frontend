4af171e654fb3cc1a58d317a09740ecf490b00a6
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2023-11-22 14:01:35 +0900

    deploy05

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,7 @@ export default defineConfig({
     host: '0.0.0.0', // ローカルネットワーク上の他のデバイスからアクセス可能にする
     port: 3000, // 好きなポート番号に設定（デフォルトは3000）
   },
+  build: {
+    chunkSizeWarningLimit: 1024, // チャンクサイズの警告限界を1024キロバイトに設定
+  }
 })
