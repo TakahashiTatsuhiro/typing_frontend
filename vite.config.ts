1437c6fb7c0d729fa5ca37aa7942d05712796e7e
tatsu <tatsuhiro_takahashi_aa@mail.toyota.co.jp>
2023-11-22 14:56:46 +0900

    deploy08

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,11 @@ export default defineConfig({
   plugins: [react()],
   // base: 'https://typing-frontend.onrender.com',
   server: {
-    host: 'https://typing-frontend.onrender.com', // ローカルネットワーク上の他のデバイスからアクセス可能にする
-    // port: 3000, // 好きなポート番号に設定（デフォルトは3000）
+    // host: 'https://typing-frontend.onrender.com', // ローカルネットワーク上の他のデバイスからアクセス可能にする
+    host: '0.0.0.0', // ローカルネットワーク上の他のデバイスからアクセス可能にする
+    port: 3000, // 好きなポート番号に設定（デフォルトは3000）
   },
-  build: {
-    chunkSizeWarningLimit: 1024, // チャンクサイズの警告限界を1024キロバイトに設定
-  }
+  // build: {
+  //   chunkSizeWarningLimit: 1024, // チャンクサイズの警告限界を1024キロバイトに設定
+  // }
 })
